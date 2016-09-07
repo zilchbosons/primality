99c9591c4a2ef7f17247272c5eac97e454ff47de
Subhendra Basu <subhendra.basu@gmail.com>
2016-09-07 20:31:24 +0530

    code

--- factorize.cpp
+++ factorize.cpp
@@ -161,45 +161,42 @@ char* factorizeN(char* nn, int* mask, int sz) {
 }
 
 void generateSums(char** matrix, char** original_matrix, int l ) {
-	mpf_t m_item;
-	mpf_t original_item;
-	mpf_t prod;
+	mpfr_t m_item;
+	mpfr_t original_item;
+	mpfr_t prod;
 	mpfr_t acc;
-	mpf_t factor;
-	mpf_init(m_item);
-	mpf_init(original_item);
-	mpf_init(prod);
-	mpf_set_ui(prod, 10);
-	mpf_init(factor);
+	mpfr_t factor;
+	mpfr_init2(m_item, 4096);
+	mpfr_init2(original_item, 4096);
+	mpfr_init(prod);
+	mpfr_set_ui(prod, 10, MPFR_RNDN);
+	mpfr_init2(factor, 4096);
 	mpfr_init2(acc, 4096);
 	mpfr_set_si(acc, 0, MPFR_RNDN);
-	mpf_init(factor);
 	mpfr_t m_term;
 	mpfr_init2(m_term, 4096);
 	mpfr_t original_term;
 	mpfr_init2(original_term, 4096);
-	for (int i = 1; i < l - 1; ++i) {
+	for (int i = 0; i < l - 1; ++i) {
 		mpfr_set_si(acc, 0, MPFR_RNDN);
 		for (int j = 0; j < l; ++j ) {
-			mpf_set_str(m_item, matrix[j], 10);
-			mpf_div(m_item, m_item, prod);
-			mpf_set_str(original_item, original_matrix[j], 10);
-			mpf_div(original_item, original_item, prod);
-			mpfr_set_f(m_term, m_item, MPFR_RNDN);
-			mpfr_log(m_term, m_term, MPFR_RNDN);
-			mpfr_set_f(original_term, original_item, MPFR_RNDN);
-			mpfr_log(original_term, original_term, MPFR_RNDN);
+			mpfr_set_str(m_item, matrix[j], 10, MPFR_RNDN);
+			mpfr_div(m_item, m_item, prod, MPFR_RNDN);
+			mpfr_set_str(original_item, original_matrix[j], 10, MPFR_RNDN);
+			mpfr_div(original_item, original_item, prod, MPFR_RNDN);
+			mpfr_log(m_term, m_item, MPFR_RNDN);
+			mpfr_log(original_term, original_item, MPFR_RNDN);
 			mpfr_mul(m_term, m_term, original_term, MPFR_RNDN);
 			mpfr_add(acc, acc, m_term, MPFR_RNDN);
-
 		}
-		mpf_mul_ui(prod, prod, 10);
+		mpfr_printf("\n%.128RNf\n", acc);
+		mpfr_mul_ui(prod, prod, 10, MPFR_RNDN);
 	}
-	mpf_clear(factor);
+	mpfr_clear(factor);
 	mpfr_clear(acc);
-	mpf_clear(prod);
-	mpf_clear(original_item);
-	mpf_clear(m_item);
+	mpfr_clear(prod);
+	mpfr_clear(original_item);
+	mpfr_clear(m_item);
 	mpfr_clear(m_term);
 	mpfr_clear(original_term);
 }
